9fd7f6bbad07e17423ec66a45106151fcce257d4
고영광 <70497430+readable-ko@users.noreply.github.com>
2023-12-25 21:38:06 +0900

    Title: 아름다운 수, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231225/아름다운 수/beautiful-number.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+int N, answer;
+map<string,bool> visited;
+string num = "";
+string str[] = {"1","22","333","4444"};
+
+void make_beauty(int depth) {
+    if(depth >= N) {
+        if(depth == N) answer++;
+        
+        return;
+    }
+    
+    for(int i = 0; i < 4; i++) {
+        num += str[i];
+        if(!visited[num]) {
+            visited[num] = 1;
+            make_beauty(depth + str[i].length());
+        }
+        num = num.substr(0,num.length() - str[i].length());
+    }
+}
+
+int main() {
+    cin >> N;
+    make_beauty(0);
+
+    cout << answer;
+    return 0;
+}
