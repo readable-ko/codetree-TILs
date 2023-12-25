b0e6cc4937d100c2589f46fa46dd298931cf59b0
고영광 <70497430+readable-ko@users.noreply.github.com>
2023-12-25 21:38:51 +0900

    Title: 다양하게 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231225/다양하게 출력/print-in-variety.cpp
+++ 231225/다양하게 출력/print-in-variety.cpp
@@ -2,9 +2,10 @@
 using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
-    cout << R"(Total days in Year
-    365
-    Circumference rate
-    3.1415926535)";
+    cout <<
+R"(Total days in Year
+365
+Circumference rate
+3.1415926535)";
     return 0;
 }
